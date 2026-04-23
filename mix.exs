16c7651b845ef2a30bc6fc1e0b8218340d94575c
thanos <thanosv@gmail.com>
2026-04-23 17:15:29 -0400

    added audit

--- mix.exs
+++ mix.exs
@@ -61,7 +61,8 @@ defmodule GraphBLAS.MixProject do
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.18", only: :test},
       {:stream_data, "~> 1.1", only: [:dev, :test]},
-      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:mix_audit, "~> 2.1", only: [:dev, :test], runtime: false}
     ]
   end
 
