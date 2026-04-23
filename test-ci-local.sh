f5c32d10cb870658dd003e2bd0cee0be7a3bcb5d
thanos <thanosv@gmail.com>
2026-04-23 14:25:16 -0400

    updated case of lib

--- test-ci-local.sh
+++ test-ci-local.sh
@@ -12,13 +12,19 @@ docker build -f "$REPO_ROOT/Dockerfile.ci" -t "$DOCKER_IMAGE" "$REPO_ROOT"
 echo ""
 echo "Running CI tests in Docker container..."
 docker run --rm \
-  -v "$REPO_ROOT:/workspace" \
+  -v "$REPO_ROOT:/src:ro" \
   -w /workspace \
   -e MIX_ENV=test \
   -e PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:/usr/local/lib64/pkgconfig" \
   "$DOCKER_IMAGE" \
   bash -c '
     set -e
+
+    # Work on a clean copy of the repo so host _build/ and deps/ from
+    # a different OTP/Elixir dont confuse protocol compilation.
+    mkdir -p /workspace
+    cp -R /src/. /workspace
+
     echo "Installing Elixir dependencies..."
     mix deps.get --force
     
